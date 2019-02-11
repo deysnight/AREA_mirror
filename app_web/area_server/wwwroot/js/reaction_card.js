9bd2f8ca455b356af55e8179bd206e4dfba1d3be
GedFenir <brice57200@live.Fr>
2019-02-11 13:19:13 +0100

    Card fix

--- app_web/area_server/wwwroot/js/reaction_card.js
+++ app_web/area_server/wwwroot/js/reaction_card.js
@@ -1,7 +1,27 @@
 var ytb_reaction_card = "<div class=\"reaction reaction_none\" id=\"ytb_reaction\">" +
 "<div class=\"reaction_card\">" +
 "<div>" +
-"<p class=\"reaction_title\">Une réaction cool</p>" +
+"<p class=\"reaction_title\">Like/Dislike</p>" +
+"<p class=\"reaction_description\">Une description cool du cul lol</p>" +
+"</div>" +
+"<hr class=\"card_separator\">" +
+"<div class=\"compatible\">" +
+"<i class=\"fa fa-envelope fa-2x\" aria-hidden=\"true\"></i>" +
+"</div>" +
+"</div>" +
+"<div class=\"reaction_card\">" +
+"<div>" +
+"<p class=\"reaction_title\">Abonnement à ma chaine</p>" +
+"<p class=\"reaction_description\">Une description cool du cul lol</p>" +
+"</div>" +
+"<hr class=\"card_separator\">" +
+"<div class=\"compatible\">" +
+"<i class=\"fa fa-envelope fa-2x\" aria-hidden=\"true\"></i>" +
+"</div>" +
+"</div>" +
+"<div class=\"reaction_card\">" +
+"<div>" +
+"<p class=\"reaction_title\">Message privé</p>" +
 "<p class=\"reaction_description\">Une description cool du cul lol</p>" +
 "</div>" +
 "<hr class=\"card_separator\">" +
@@ -12,6 +32,16 @@ var ytb_reaction_card = "<div class=\"reaction reaction_none\" id=\"ytb_reaction
 "</div>";
 
 var fb_reaction_card = "<div class=\"reaction reaction_none\" id=\"fb_reaction\">" +
+"<div class=\"reaction_card\">" +
+"<div>" +
+"<p class=\"reaction_title\">Message privé</p>" +
+"<p class=\"reaction_description\">Une description cool du cul lol</p>" +
+"</div>" +
+"<hr class=\"card_separator\">" +
+"<div class=\"compatible\">" +
+"<i class=\"fa fa-envelope fa-2x\" aria-hidden=\"true\"></i>" +
+"</div>" +
+"</div>" +
 "<div class=\"reaction_card\">" +
 "<div>" +
 "<p class=\"reaction_title\">Une réaction cool</p>" +
@@ -27,8 +57,8 @@ var fb_reaction_card = "<div class=\"reaction reaction_none\" id=\"fb_reaction\"
 var twitch_reaction_card = "<div class=\"reaction reaction_none\" id=\"twitch_reaction\">" +
 "<div class=\"reaction_card\">" +
 "<div>" +
-"<p class=\"reaction_title\">Une réaction cool</p>" +
-"<p class=\"reaction_description\">Une description cool du cul lol</p>" +
+"<p class=\"reaction_title\">Streamer Online</p>" +
+"<p class=\"reaction_description\">Soyez notifié quand votre streamer est en ligne</p>" +
 "</div>" +
 "<hr class=\"card_separator\">" +
 "<div class=\"compatible\">" +
@@ -53,12 +83,12 @@ var slack_reaction_card = "<div class=\"reaction reaction_none\" id=\"slack_reac
 var gmail_reaction_card = "<div class=\"reaction reaction_none\" id=\"gmail_reaction\">" +
 "<div class=\"reaction_card\">" +
 "<div>" +
-"<p class=\"reaction_title\">Une réaction cool</p>" +
-"<p class=\"reaction_description\">Une description cool du cul lol</p>" +
+"<p class=\"reaction_title\">Upload pièce jointe</p>" +
+"<p class=\"reaction_description\">Mon cul sur la chaise</p>" +
 "</div>" +
 "<hr class=\"card_separator\">" +
 "<div class=\"compatible\">" +
-"<i class=\"fa fa-envelope fa-2x\" aria-hidden=\"true\"></i>" +
+"<i class=\"fa fa fa-cloud fa-2x\" aria-hidden=\"true\"></i>" +
 "</div>" +
 "</div>" +
 "</div>";
@@ -76,9 +106,9 @@ var onedrive_reaction_card = "<div class=\"reaction reaction_none\" id=\"onedriv
 "</div>" +
 "</div>";
 
-$('#card-wrap').append(ytb_reaction_card);
-$('#card-wrap').append(fb_reaction_card);
-$('#card-wrap').append(twitch_reaction_card);
-$('#card-wrap').append(slack_reaction_card);
-$('#card-wrap').append(gmail_reaction_card);
-$('#card-wrap').append(onedrive_reaction_card);
+$('#action-reaction-wrap').append(ytb_reaction_card);
+$('#action-reaction-wrap').append(fb_reaction_card);
+$('#action-reaction-wrap').append(twitch_reaction_card);
+$('#action-reaction-wrap').append(slack_reaction_card);
+$('#action-reaction-wrap').append(gmail_reaction_card);
+$('#action-reaction-wrap').append(onedrive_reaction_card);
