e1af2b9ef35d93cf7ea57ff20674eb7827200a5d
GedFenir <brice57200@live.Fr>
2019-02-12 18:22:57 +0100

    OAuth with Facebook

--- app_web/area_server/wwwroot/js/reaction_card.js
+++ app_web/area_server/wwwroot/js/reaction_card.js
@@ -126,21 +126,83 @@ var onedrive_reaction_card = "<div class=\"reaction reaction_none\" id=\"onedriv
 "</div>" +
 "</div>";
 
-var twitch_login = 
+var twitch_login =
 "<div class=\"reaction reaction_none\" id=\"twitch_reaction\">" +
-"<div class=\"login_button_container\">" +
+"<div class=\"login_button_container_twitch\">" +
 "<a href=\"https://api.twitch.tv/kraken/oauth2/authorize?response_type=code&client_id=bdxjm36z6d15f9barb92e8kiogru3t&redirect_uri=http://charlespd.com:8080/&scope=user_follows_edit\">Login Twitch</a>" +
 "</div>"
 "</div>";
 
+var fb_login =
+"<div class=\"reaction reaction_none\" id=\"fb_reaction\">" +
+"<div class=\"login_button_container_fb\">" +
+"<a onclick=\"initiateFBLogin();\" href=\"#\">Login Facebook</a>" +
+"</div>"
+"</div>";
+
 var teste = false;
+var teste_fb = true;
 
 $('#action-reaction-wrap').append(ytb_reaction_card);
-$('#action-reaction-wrap').append(fb_reaction_card);
-if (teste == true)
+
+if (teste_fb == false) {
+    $('#action-reaction-wrap').append(fb_reaction_card);
+}
+else {
+    $('#action-reaction-wrap').append(fb_login);
+}
+
+
+if (teste == false) {
     $('#action-reaction-wrap').append(twitch_reaction_card);
-else
+}
+else {
     $('#action-reaction-wrap').append(twitch_login);
+}
+
+
+
 $('#action-reaction-wrap').append(slack_reaction_card);
 $('#action-reaction-wrap').append(gmail_reaction_card);
 $('#action-reaction-wrap').append(onedrive_reaction_card);
+
+
+function statusChangeCallback(response) {
+    if (response.status === 'connected') {
+    }
+    else if (response.status === 'not_authorized') {
+    }
+    else {
+    }
+}
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+}
+
+function fetch_user_data() {
+    FB.api('/me/accounts', function(response) {
+        console.log(response.data);
+})};
+
+function initiateFBLogin() {
+    FB.login(function(response) {
+        send_FB_data_to_serv(response);
+    }, {scope: 'public_profile,email,manage_pages'});
+}
+
+function send_FB_data_to_serv(response) {
+   var id_token = response.authResponse.accessToken
+    $.ajax({
+    url: "/internal/oauth2/facebook ",
+    method: "POST",
+    data: {
+        user_id_token: id_token,
+    },
+    success: function (response) {
+        location.reload();
+    }
+});
+}
