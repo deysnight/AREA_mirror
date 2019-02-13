e2eaf52e37de011ed3c618d65d18a33ffa915953
Unknown <louison.harizi@epitech.eu>
2019-02-13 15:56:52 +0100

    confirmation ok op etc

--- app_web/area_server/Controllers/PagesController.cs
+++ app_web/area_server/Controllers/PagesController.cs
@@ -10,6 +10,39 @@ namespace area_server.Controllers
 {
     public class PagesController : ControllerBase
     {
+        [Route("test")]
+        [HttpGet]
+        public ActionResult<string> test()
+        {
+            Utils.send_mail_confirmation("moimoi", "louison.harizi@epitech.eu");
+            return "test page";
+        }
+
+        [Route("auth/activate/{data}")]
+        [HttpGet]
+        public ActionResult<string> mail_activate(string data)
+        {
+            string decode = Utils.read_token(data);
+            string[] info = decode.Split(":");
+            dynamic json = (Newtonsoft.Json.Linq.JArray)DBConnect.my_select("users WHERE id = " + int.Parse(info[0]) + " AND login = '" + info[1] + "' AND email = '" + info[2] + "' AND isOAuth = 0", "*");
+            if (json.Count == 0) { Response.Redirect("/login?statut=error user no exist"); return ""; }
+            if (json.Count >= 0) { Response.Redirect("/login?statut=error multiple user"); return ""; }
+            if (json[0].isValidated == 0)
+            {
+                DBConnect.my_update("users", "isValidated = 1", "id = " + info[0]);
+                Response.Redirect("/login?statut=user_validated");
+                return ("");
+            }
+            else
+            {
+                Response.Redirect("/login?statut=user_already_validated");
+                return ("");
+            }
+            //should never reach here
+            Response.Redirect("/login?statut=user_validated");
+            return ("");
+        }
+
         [Route("")]
         [HttpGet]
         public ActionResult<string> Home()
