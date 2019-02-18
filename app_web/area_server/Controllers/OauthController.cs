15457e5823b8786e838449bf09d9578dfc451e8f
Unknown <louison.harizi@epitech.eu>
2019-02-18 14:29:11 +0100

    need les html la

---
+++ app_web/area_server/Controllers/OauthController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace area_server.Controllers
+{
+    [Route("/internal/oauth2")]
+    public class OauthController : Controller
+    {
+        [HttpGet("twitch")]
+        public ActionResult<string> token_twitch()
+        {
+            string url = Request.GetDisplayUrl();
+            string[] url_splited = url.Split(new char[] { '=', '&' });
+            string token = url_splited[1];
+
+            return (token);
+        }
+
+        [HttpGet("onedrive")]
+        public ActionResult<string> token_OFFICE_LES_PUTES()
+        {
+            string url = Request.GetDisplayUrl();
+            string[] url_splited = url.Split(new char[] { '=', '&' });
+            string token = url_splited[1];
+
+            return (token);
+        }
+    }
+}
