ba9709604332b29427dc242b76beac3d747f0215
Unknown <charles.claude@epitech.eu>
2019-02-22 12:19:25 +0100

    Oauth google done

--- app_mobile/AreaMobileApp/component/LoginForm.js
+++ app_mobile/AreaMobileApp/component/LoginForm.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { StyleSheet, AsyncStorage, View, TextInput, TouchableOpacity, Text } from 'react-native';
+import {AuthSession} from 'expo'
+
 
 export default class LoginForm extends Component {
 
-    constructor (props) {
-        super(props)
-    }
+        state = {
+            result: null,
+        };
+
+        _signIn = async () => {
+
+            let redirectUrl = AuthSession.getRedirectUrl();
+            const result = await Expo.Google.logInAsync({
+            androidClientId: "218486788454-is3b9o2utppp7mfjonuhnd9v2kp74ve9.apps.googleusercontent.com",
+            iosClientId: "218486788454-is3b9o2utppp7mfjonuhnd9v2kp74ve9.apps.googleusercontent.com",
+            behavior: 'web',
+            scopes: ["profile", "email", "https://mail.google.com/", "https://www.googleapis.com/auth/youtube.force-ssl", "https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/spreadsheets"]
+            })
+            if (result.type === "success") {
+                console.log(result.accessToken);
+                var google_token = result.accessToken;
+                //Redirect to app;
+            } else {
+                console.log("cancelled")   
+            }
+        }
 
     render() {
         return (
@@ -26,34 +46,38 @@ export default class LoginForm extends Component {
                 style={styles.input}
                 ref={(input) => this.passwordInput = input}
                 />
-            <TouchableOpacity style={styles.buttonContainer}
-                
-                >
+            <TouchableOpacity style={styles.buttonContainer}>
                 <Text style={styles.buttonText}>Se connecter</Text>
             </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonContainer} onPress={this._signIn}>
+                <Text style={styles.buttonText}>Se connecter via Google</Text>
+            </TouchableOpacity>
+            {this.state.result ? (
+		    <Text> {this.access_token} </Text>
+            ) : null}
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-   container: {
-       padding: 20
-   },
-   input: {
+    container: {
+    padding: 20
+    },
+    input: {
     height: 40,
     backgroundColor: 'rgba(255, 255, 255, 0.2)',
     marginBottom: 20,
     color: '#FFF',
     paddingHorizontal: 10
-   },
-   buttonContainer: {
-       backgroundColor: '#2980b9',
-       paddingVertical: 15
-   },
-   buttonText: {
+    },
+    buttonContainer: {
+        backgroundColor: '#2980b9',
+        paddingVertical: 15
+    },
+    buttonText: {
     textAlign: 'center',
     color: '#FFFFFF',
     fontWeight: '700'
-   }
+    }
 });
