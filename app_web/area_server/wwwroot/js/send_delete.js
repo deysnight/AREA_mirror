53f89ef6cd79ec268706697f3c3e4da0a2176af5
GedFenir <brice57200@live.Fr>
2019-02-25 00:12:58 +0100

    Delete request done

--- app_web/area_server/wwwroot/js/send_delete.js
+++ app_web/area_server/wwwroot/js/send_delete.js
@@ -13,3 +13,382 @@ function send_delete_ytb_1_1() {
         }
     })
 };
+function send_delete_ytb_1_2() {
+    $("#youtube_first_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_1_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_first_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_ytb_1_3() {
+    $("#youtube_first_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_1_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_first_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_ytb_2_1() {
+    $("#youtube_second_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_2_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_second_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_ytb_2_2() {
+    $("#youtube_second_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_2_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_second_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_ytb_2_3() {
+    $("#youtube_second_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_2_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_second_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_ytb_3_1() {
+    $("#youtube_third_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_3_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_third_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_ytb_3_3() {
+    $("#youtube_third_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "ytb_3_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#youtube_third_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_fb_1_1() {
+    $("#facebook_first_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_1_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_first_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_1_2() {
+    $("#facebook_first_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_1_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_first_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_1_3() {
+    $("#facebook_first_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_1_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_first_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_2_1() {
+    $("#facebook_second_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_2_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_second_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_2_2() {
+    $("#facebook_second_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_2_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_first_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_2_3() {
+    $("#facebook_second_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_2_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_second_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_3_1() {
+    $("#facebook_third_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_3_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_third_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_3_2() {
+    $("#facebook_third_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_3_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_third_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_fb_3_3() {
+    $("#facebook_third_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "fb_3_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#facebook_third_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_twitch_1_1() {
+    $("#twitch_first_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_1_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_first_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_1_2() {
+    $("#twitch_first_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_1_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_first_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_1_3() {
+    $("#twitch_first_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_1_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_first_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_2_1() {
+    $("#twitch_second_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_2_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_second_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_2_2() {
+    $("#twitch_second_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_2_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_second_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_2_3() {
+    $("#twitch_second_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_2_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_second_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_3_1() {
+    $("#twitch_third_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_3_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_third_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_twitch_3_3() {
+    $("#twitch_third_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "twitch_3_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#twitch_third_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_onedrive_1_1() {
+    $("#onedrive_first_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "onedrive_1_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#onedrive_first_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_onedrive_1_2() {
+    $("#onedrive_first_sheet").css("display", "none");
+    $.ajax({
+        url: final_url + "onedrive_1_2",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#onedrive_first_sheet").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_onedrive_1_3() {
+    $("#onedrive_first_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "onedrive_1_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#onedrive_first_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_gdrive_1_1() {
+    $("#gdrive_first_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "gdrive_1_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#gdrive_first_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_gdrive_1_3() {
+    $("#gdrive_first_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "gdrive_1_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#gdrive_first_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
+
+function send_delete_gsheet_1_1() {
+    $("#gsheet_first_mail").css("display", "none");
+    $.ajax({
+        url: final_url + "gsheet_1_1",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#gsheet_first_mail").css("display", "none");
+            location.reload();
+        }
+    })
+};
+function send_delete_gsheet_1_3() {
+    $("#gsheet_first_facebook").css("display", "none");
+    $.ajax({
+        url: final_url + "gsheet_1_3",
+        method: type,
+        async: false,
+        success: function (response) {
+            $("#gsheet_first_facebook").css("display", "none");
+            location.reload();
+        }
+    })
+};
